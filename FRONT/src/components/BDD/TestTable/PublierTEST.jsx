974dc224f59558e2e510f6c001b2fe2e482dde5e
Romain Viravaud <romainviravaud@gmail.com>
2023-02-14 21:30:06 +0100

    francesco-update-

--- FRONT/src/components/BDD/TestTable/PublierTEST.jsx
+++ FRONT/src/components/BDD/TestTable/PublierTEST.jsx
@@ -1,37 +1,51 @@
 import "./styles.css";
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
+
+
 export default function PublierTEST() {
   const [data, setData] = useState(null);
   const [chargement, setChargement] = useState(true);
   const [error, setError] = useState(null);
   const [search, setSearch] = useState("");
-  const [donation, setDonation] = useState([]);
+  
+//select et quantité
+  const [numero, setNumero] = useState({ quantity: 0, selected: 0 });
+
+  const { id } = useParams();
 
   useEffect(() => {
     getData();
   }, []);
 
-  //AJOUTERr
-  const handleIncrease = (cardid) => {
-    setDonation((donation) =>
-      donation.map((product) =>
-        cardid === product.id
-          ? { ...product, product_qty: product.quantity - 1 }
-          : product
-      )
+  //bouton publier
+  async function onSubmit(e) {
+    e.preventDefault();
+    const requestOptions = {
+      method: "POST", 
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        quantity: numero.quantity,
+        selected: numero.selected,
+      }),
+    };
+    let response = await fetch(
+      `http://localhost:3000/product/${id}`,
+      requestOptions
     );
-  };
+    let json = await response.json();
+    setNumero(json);
+    console.log(json);
+  }
 
-  const handleDecrease = (cardid) => {
-    setDonation((donation) =>
-      donation.map((product) =>
-        cardid === product.id
-          ? { ...product, product_qty: product.quantity + 1 }
-          : product
-      )
-    );
-  };
+  
+  function handleForm(e) {
+    const newData = { ...data };
+    newData[e.target.id] = e.target.value;
+    setNumero(newData);
+    console.log(newData);
+  }
 
   //FETCH PRODUITS
   async function getData() {
@@ -56,7 +70,7 @@ export default function PublierTEST() {
     <>
       <label htmlFor="search">
         <input
-          placeholder="Cherchez un prodit par référence"
+          placeholder="Cherchez un produit par référence"
           id="search"
           type="text"
           onChange={(e) => setSearch(e.target.value)}
@@ -81,32 +95,29 @@ export default function PublierTEST() {
             .map((product) => (
               <tr key={product.id}>
                 <td>
-                  <input type="checkbox" />
+                  <input
+                    type="checkbox"
+                    value={numero.selected}
+                    id={product.id}
+                  />
                 </td>
                 <td>{product.reference}</td>
                 <td>{product.description}</td>
                 <td>
                   <div className="quantity">
-                    <button
-                      type="button"
-                      onClick={() => handleDecrease(product)}
-                    >
-                      -
-                    </button>
-                    <input type="number" />
-                    {product.quantity}
-                    <button
-                      type="button"
-                      onClick={() => handleIncrease(product)}
-                    >
-                      +
-                    </button>
+                    <input
+                      type="number"
+                      id={product.id}
+                      value={numero.quantity}
+                      onChange={handleForm}
+                    />
                   </div>
                 </td>
               </tr>
             ))}
         </tbody>
       </table>
+      <button onClick={onSubmit}> Publier </button>
     </>
   );
 }
