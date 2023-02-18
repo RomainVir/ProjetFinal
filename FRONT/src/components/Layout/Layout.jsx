53b43158d7ea23bee451a26b6330b8a8b93e8653
Romain Viravaud <romainviravaud@gmail.com>
2023-02-18 09:44:13 +0100

    ok samedi

--- FRONT/src/components/Layout/Layout.jsx
+++ FRONT/src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import { HomeMenu } from "../../const/HomeMenu";
-import Footer from "../Footer/Footer";
+//import Footer from "../Footer/Footer";
 
 import Header from "../Header/Header";
 import NavBar from "../Header/NavBar";
@@ -12,7 +12,7 @@ export default function Layout() {
       <NavBar menuItems={HomeMenu} />
 
       <Outlet />
-      <Footer />
+     
     </>
   );
 }
