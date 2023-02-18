dbfdc1e7a9373a97ed890bede5043af055ea28b0
Romain Viravaud <romainviravaud@gmail.com>
2023-02-18 17:45:30 +0100

    ok

--- FRONT/src/components/PDF/Pdf.jsx
+++ FRONT/src/components/PDF/Pdf.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
-import { Input } from "antd";
 
 const styles = StyleSheet.create({
   page: {
