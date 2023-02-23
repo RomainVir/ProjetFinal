83c8d90c9bf1bfe93287a3f2fae604fadd8a48e3
Romain Viravaud <romainviravaud@gmail.com>
2023-02-23 13:06:51 +0100

    mon compte auto complete

--- FRONT/src/components/ENTREPRISE/MonCompte/TestCompte.jsx
+++ FRONT/src/components/ENTREPRISE/MonCompte/TestCompte.jsx
@@ -1,139 +1,44 @@
-import { useState } from "react";
-import Swal from "sweetalert2";
-import { useParams } from "react-router";
+import { useAuthContext } from "../../../context/AuthContext";
+import React, { useState, useEffect } from "react";
 
-const initialUserState = {
-  companyName: "",
-  contactSurname: "",
-  contactName: "",
-  email: "",
-  phone: "",
-  address: "",
-  postalCode: "",
-  town: "",
-  password: "",
-  confirmPassword: "",
-};
 export default function ModifierCompte() {
-  const utilisateur = useParams();
+  const [data, setData] = useState("");
+  const { authorization } = useAuthContext();
 
-  const [newModif, setNewModif] = useState(initialUserState);
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch(
+        `http://localhost:3000/user/users/${authorization.id}`
+      );
+      setData(await response.json());
+    }
+    getData();
+    console.log(data);
+  }, []);
 
-  function handleInput(event) {
-    const Modification = {
-      ...newModif,
-      [event.target.name]: event.target.value,
-    };
-
-    setNewModif(Modification);
-  }
-
-  async function onSubmit(e) {
-    e.preventDefault();
-    console.log(newModif);
-    fetch(`http://localhost:3000/user/${utilisateur.id}`, {
-      method: "PATCH",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newModif),
-    }).then((response) => {
-      console.log(response.status);
-      if (response.status === 401) {
-        throw "Non autorisé";
-      } else if (response.status === 200) {
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: `Utilisateur ${newModif.companyName} modifié correctement`,
-          showConfirmButton: false,
-          timer: 2000,
-        });
-        setNewModif(initialUserState);
-      } else if (response.status === 409) {
-        Swal.fire({
-          position: "center",
-          icon: "error",
-          title: `Utilisateur ${newModif.companyName} déja modifié`,
-          showConfirmButton: false,
-          timer: 2000,
-        });
-      }
-    });
-  }
   return (
     <div className="register">
-      <form className="formRegister" onSubmit={onSubmit}>
+      <form className="formRegister">
         <h1>Modifier vos informations</h1>
+        <input type="text" name="companyName" value={data.companyName} />
         <input
           type="text"
-          name="companyName"
-          placeholder="Entreprise"
-          value={newModif.companyName}
-          onChange={handleInput}
-        />
-        <input
-          type="text"
+          value={data.contactSurname}
           name="contactSurname"
           required
-          placeholder="Nom du contact"
-          value={newModif.contactSurname}
-          onChange={handleInput}
         />
-        <input
-          type="text"
-          name="contactName"
-          placeholder="Prénom du contact"
-          value={newModif.contactName}
-          onChange={handleInput}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email"
-          value={newModif.email}
-          onChange={handleInput}
-        />
-        <input
-          type="text"
-          placeholder="Téléphone"
-          name="phone"
-          value={newModif.phone}
-          onChange={handleInput}
-        />
-        <input
-          type="text"
-          placeholder="Adresse"
-          name="address"
-          value={newModif.address}
-          onChange={handleInput}
-        />
-        <input
-          type="text"
-          placeholder="Code Postal"
-          name="postalCode"
-          value={newModif.postalCode}
-          onChange={handleInput}
-        />
-        <input
-          type="text"
-          placeholder="Ville"
-          name="town"
-          value={newModif.town}
-          onChange={handleInput}
-        />
+        <input type="text" name="contactName" value={data.contactName} />
+        <input type="email" value={data.email} name="email" />
+        <input type="text" value={data.phone} name="phone" />
+        <input type="text" value={data.address} name="address" />
+        <input type="text" value={data.postalCode} name="postalCode" />
+        <input type="text" value={data.town} name="town" />
         <br />
-        <input
-          type="password"
-          placeholder="Mot de passe"
-          name="password"
-          value={newModif.password}
-          onChange={handleInput}
-        />
+        <input type="password" value={data.password} name="password" />
         <input
           type="password"
           placeholder="Confirmez votre mot de passe"
           name="confirmPassword"
-          value={newModif.confirmPassword}
-          onChange={handleInput}
         />
 
         <button type="submit">Modifier</button>
--- FRONT/src/views/ViewPDF/ViewPdf.jsx
+++ FRONT/src/views/ViewPDF/ViewPdf.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useAuthContext } from "../../context/AuthContext";
 import "./pdf.css";
+import Deliveries from "../Deliveries/Deliveries";
 
 export default function Pdf() {
   const [data, setData] = useState("");
@@ -21,38 +22,44 @@ export default function Pdf() {
 
   return (
     <>
-      {data === undefined ? (
-        <p>Cargando...</p>
-      ) : (
-        <form>
-          <h1>
-            Reçu au titre des dons à certains organismes d’intérêt général{" "}
-            <br />
-            Article 200, 238 bis et 978 du code général des impôts (CGI)
-          </h1>
-          <div className="formulaire">
-            <h2>Bénéficiaire des versements</h2>
-            <input type="text" value={data.companyName} />
-            <div>
-              <h4>Adresse</h4>
-              <input type="text" value={data.address} />
-              <input type="text" value={data.postalCode} />
-              <input type="text" value={data.town} />
-              <input type="text" value={data.type} />
+      <div className="bilan">
+        {data === undefined ? (
+          <p>Cargando...</p>
+        ) : (
+          <form>
+            <div className="formulaire">
+            <h3>
+              Reçu au titre des dons à certains organismes d’intérêt général{" "}
               <br />
+              Article 200, 238 bis et 978 du code général des impôts (CGI)
+            </h3>
+              <h4>Bénéficiaire des versements</h4>
+              <input type="text" value={data.companyName} />
+              <div>
+                <h4>Adresse</h4>
+                <input type="text" value={data.address} />
+                <input type="text" value={data.postalCode} />
+                <input type="text" value={data.town} />
+                <input type="text" value={data.type} />
+                <br />
+              </div>
+              <br />
+              <textarea
+                name="texte"
+                id=""
+                cols="60"
+                rows="5"
+                placeholder="Commentaire..."
+              ></textarea>
             </div>
-            <br />
-            <textarea
-              name="texte"
-              id=""
-              cols="60"
-              rows="5"
-              placeholder="Commentaire..."
-            ></textarea>
-            <button>Envoyer le formulaire</button>
-          </div>
-        </form>
-      )}
+          </form>
+        )}
+
+        <div className="deliveries">
+          <Deliveries />
+        </div>
+      </div>
+        <button className="envoyer">Envoyer le formulaire</button>
     </>
   );
 }
